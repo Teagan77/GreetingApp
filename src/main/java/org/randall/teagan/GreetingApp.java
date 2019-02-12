c84800d406c5ea07921e98ec1f99ce3e49a8f1fa
Teagan <randallteagan@gmail.com>
2019-02-12 20:37:14 +0200

    Initial commit

---
+++ src/main/java/org/randall/teagan/GreetingApp.java
@@ -0,0 +1,17 @@
+package org.randall.teagan;
+
+import java.util.Scanner;
+
+public class GreetingApp{
+
+    public static void main(String[] args){
+        greet();
+    }
+
+    protected static void greet(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = scanner.next();
+        System.out.println("Welcome " + name + ", to the world of Java programming!");
+    }
+}
---
+++ src/test/java/org/randall/teagan/GreetingAppTest.java
@@ -0,0 +1,14 @@
+package org.randall.teagan;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class GreetingAppTest
+{
+    @Test
+    public void greetingTest()
+    {
+        assertTrue( true );
+    }
+}
